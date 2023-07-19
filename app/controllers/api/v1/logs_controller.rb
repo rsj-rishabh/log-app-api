018696f3ac0187e3625aaf5fc7ecf86b1e3c8d65
Rishabh Jaiswal <32019969+rsj-rishabh@users.noreply.github.com>
2023-07-19 16:43:37 -0700

    Added ordering in paginated response

--- app/controllers/api/v1/logs_controller.rb
+++ app/controllers/api/v1/logs_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::LogsController < ApplicationController
   def index
     
-    @logs = LogDatum.paginate(page: params[:page], per_page: 10)
+    @logs = LogDatum.reorder('log_index ASC').paginate(page: params[:page], per_page: 10)
     total_pages = @logs.total_pages
 
     response.headers['X-Total-Pages'] = total_pages.to_s
