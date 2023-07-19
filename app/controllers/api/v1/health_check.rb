9a42969940be956b3e80b09c117f6750e8f4c4a3
Rishabh Jaiswal <32019969+rsj-rishabh@users.noreply.github.com>
2023-07-19 16:44:18 -0700

    Fixed health_check.rb

--- app/controllers/api/v1/health_check.rb
+++ app/controllers/api/v1/health_check.rb
@@ -1,8 +1,11 @@
-class HealthCheckController < ApplicationController
-    rescue_from(Exception) { render head: 503 }
-  
-    def show
-      render head: 200
-    end
-    
+class Api::V1::HealthCheckController < ApplicationController
+  rescue_from(Exception) { render head: 503 }
+
+  def index
+    render head: 200
+  end
+
+  def show
+    render head: 200
+  end
 end
